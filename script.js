4fa4aaa5401365c4de2135a5f33fd26a1cf9319e
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-01-28 19:53:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+ ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
       .then(registration => {
